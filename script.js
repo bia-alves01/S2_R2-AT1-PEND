6658722647fa97896a1962c44e0acfc91031ac39
BEATRIZ VASCONCELOS ALVES <beatriz.alves39@senaisp.edu.br>
2026-03-13 16:15:35 -0300

    Exercicio 04 incompleta

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 let input = document.getElementById("item");
 let btnListar = document.querySelector("#btnListar");
-//let mensagem = document.getElementById("mensagem");
+let mensagem = document.getElementById("mensagem");
 let lista = document.getElementById("Lista")
 
-let complemento = input.value;
+//let complemento = input.value;
 
 btnListar.addEventListener("click", () => {
 
     if (!input.value) {
-        mensagem.className = "text-danger";
-        mensagem.innerText = "Digite um item!";
+        mensagem.innerText = "Tarefa vazio!";
+    }else{
+        let itemNovo = document.createElement("li");
+        itemNovo.innerText = input.value
+        lista.appendChild(itemNovo)
+        mensagem.innerText = `Criado com sucesso`
+    
+        input.value = "";
     }
     
-    const li = document.createElement("li");
-    li.innerText = input.value;
-    lista.appendChild(li)
-
-    input.value = "";
     // mensagem.innerText = `${lista}`;   
 
 })
