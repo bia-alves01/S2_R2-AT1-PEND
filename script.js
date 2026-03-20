e6d7e4ec1072ad3c73982c7fc946b5a3febf84b1
BEATRIZ VASCONCELOS ALVES <beatriz.alves39@senaisp.edu.br>
2026-03-20 15:27:23 -0300

    Exercicio 06 e Atividade Finalizada

--- script.js
+++ script.js
@@ -8,38 +8,53 @@ let lista = document.getElementById("listaTarefas")
 btnAdicionar.addEventListener("click", () => {
 
     if (!input.value) {
-        mensagem.innerText = "Tarefa vazio!";
+        mensagem.innerText = "Tarefa vazia!";
     } else {
         //Criar um item 
         let itemNovo = document.createElement("li");
-        itemNovo.className = "list-group-item   "
-        itemNovo.innerText = input.value
-        listaTarefas.appendChild(itemNovo)
-        mensagem.innerText = `Criado com sucesso`
+        itemNovo.className = "list-group-item d-flex justify-content-between align-items-center"
+        
+        let divLabel = document.createElement("div");
+        divLabel.className = "d-flex justify-content-between align-items-center";
+
+        //<Div> do checkout
+        let divInput = document.createElement("div");
+        divInput.className = "form-check d-flex align-items-center";
+        
+        //Criar um check
+        let check = document.createElement("input")
+        check.className = "form-check-input"
+        check.setAttribute("type",'checkbox');
+        divInput.appendChild(check); 
+        divLabel.appendChild(divInput);
+        
+        // Texto do Item
+        let p = document.createElement("p");
+        p.innerText = input.value;
+        p.className = "mb-0";
+        divLabel.appendChild(p);
 
         input.value = "";
-
+        
         //Criar botão de remover
         let btnRemover = document.createElement("button")
         btnRemover.innerText = 'Remover';
         btnRemover.className = "btn btn-outline-danger m-3"
-        itemNovo.appendChild(btnRemover);
-
+        
+        
         //Criar função do botão remover
         btnRemover.addEventListener("click", ()=> {
             itemNovo.remove();
+            btnRemover.remove();
         })
-
-        itemNovo.appendChild(itemNovo);
-
-        //Criar um check
-        let check = dcument.createElement("checkbox")
-        check.className = "form-check"
-        itemNovo.appendChild(check);
-
-
+        
+        itemNovo.appendChild(divLabel);
+        itemNovo.appendChild(btnRemover);
+        lista.appendChild(itemNovo);
+        
+        mensagem.innerText = `Criado com sucesso`
     }
     
-
-
+    
+    
 })
