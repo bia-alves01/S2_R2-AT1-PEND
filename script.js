f529dc8f2c34ff48f506ff3c27fba8909a55dc2f
BEATRIZ VASCONCELOS ALVES <beatriz.alves39@senaisp.edu.br>
2026-03-20 09:35:20 -0300

    Exercicio 5

--- script.js
+++ script.js
@@ -9,13 +9,28 @@ btnListar.addEventListener("click", () => {
 
     if (!input.value) {
         mensagem.innerText = "Tarefa vazio!";
-    }else{
+    } else {
+        //Criar um item 
         let itemNovo = document.createElement("li");
         itemNovo.innerText = input.value
         lista.appendChild(itemNovo)
         mensagem.innerText = `Criado com sucesso`
-    
+
         input.value = "";
+
+        //Criar botão de remover
+        let btnRemover = document.createElement("button")
+        btnRemover.innerText = 'Remover';
+        btnRemover.className = "btn btn-outline-danger m-3"
+        itemNovo.appendChild(btnRemover);
+
+        btnRemover.addEventListener("click", ()=> {
+            itemNovo.remove();
+        })
+
+        itemNovo.appendChild(itemNovo);
     }
+    
+
 
 })
